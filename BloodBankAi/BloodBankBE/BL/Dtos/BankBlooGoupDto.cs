cf2ef641a3a954af22b5571e02746436f771e63e
mohamed ashraf <moashraf20117@gmail.com>
2023-03-30 15:46:28 +0200

    remove project

--- BloodBankAi/BloodBankBE/BL/Dtos/BankBlooGoupDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BL.Dtos
-{
-    public class BankBlooGoupDto
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public ICollection<BloodGroupDto>? BloodGroups { get; set; }
-    }
-}
--- BloodBankAi/BloodBankBE/BL/Dtos/GovernorateDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BL.Dtos
-{
-    public class GovernorateDto
-    {
-        public int? Id { get; set; }
-        public string? Name { get; set; }
-    }
-}
